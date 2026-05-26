91ba400588933702004539d5399464a6614dd362
srdjanjovic <164680157+srdjanjovic@users.noreply.github.com>
2026-05-26 15:31:56 +0200

    kreiran projekat i spojen na erureku

---
+++ analiza-aktivnosti/src/test/java/com/example/analiza_aktivnosti/AnalizaAktivnostiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analiza_aktivnosti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalizaAktivnostiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
